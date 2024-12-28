e754dd9ced7a7cf1f388d4bd5136f397fae2e71b
Garrett Harkness <gharkness@sourceallies.com>
2024-12-28 12:39:03 -0600

    init commit

---
+++ src/test/java/com/gharkness/sdjpainheritance/SdjpaInheritanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gharkness.sdjpainheritance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaInheritanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
